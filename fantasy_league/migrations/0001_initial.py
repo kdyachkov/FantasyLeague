8930832040f833f3bacd8e6ad90e5db602be572e
Konstantin Dyachkov <kdyachkov@gmail.com>
2013-12-08 00:15:00 -0500

    re-initializing the app, no need to keep old migrations

--- fantasy_league/migrations/0001_initial.py
+++ fantasy_league/migrations/0001_initial.py
@@ -20,8 +20,8 @@ class Migration(SchemaMigration):
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('primary_position', self.gf('django.db.models.fields.related.ForeignKey')(related_name='primary_position', to=orm['fantasy_league.Position'])),
-            ('init_value', self.gf('django.db.models.fields.DecimalField')(max_digits=3, decimal_places=2)),
-            ('current_value', self.gf('django.db.models.fields.DecimalField')(max_digits=3, decimal_places=2)),
+            ('init_value', self.gf('django.db.models.fields.DecimalField')(max_digits=4, decimal_places=2)),
+            ('current_value', self.gf('django.db.models.fields.DecimalField')(max_digits=4, decimal_places=2)),
         ))
         db.send_create_signal(u'fantasy_league', ['Player'])
 
@@ -38,7 +38,8 @@ class Migration(SchemaMigration):
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100)),
             ('captain', self.gf('django.db.models.fields.related.ForeignKey')(related_name='captain', to=orm['fantasy_league.Player'])),
-            ('team_points', self.gf('django.db.models.fields.DecimalField')(max_digits=4, decimal_places=2)),
+            ('team_points', self.gf('django.db.models.fields.DecimalField')(max_digits=6, decimal_places=2)),
+            ('money_to_spend', self.gf('django.db.models.fields.DecimalField')(max_digits=6, decimal_places=2)),
         ))
         db.send_create_signal(u'fantasy_league', ['Team'])
 
@@ -48,7 +49,7 @@ class Migration(SchemaMigration):
             ('player', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['fantasy_league.Player'])),
             ('position', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['fantasy_league.Position'])),
             ('team', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['fantasy_league.Team'])),
-            ('amount_paid', self.gf('django.db.models.fields.DecimalField')(max_digits=3, decimal_places=2)),
+            ('amount_paid', self.gf('django.db.models.fields.DecimalField')(max_digits=4, decimal_places=2)),
             ('date_picked', self.gf('django.db.models.fields.DateField')()),
         ))
         db.send_create_signal(u'fantasy_league', ['Membership'])
@@ -56,10 +57,35 @@ class Migration(SchemaMigration):
         # Adding model 'User'
         db.create_table(u'fantasy_league_user', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('team', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['fantasy_league.Team'])),
+            ('password', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('last_login', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('is_superuser', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('username', self.gf('django.db.models.fields.CharField')(unique=True, max_length=254)),
+            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, blank=True)),
+            ('is_staff', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('date_joined', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('team', self.gf('django.db.models.fields.related.ForeignKey')(default=None, to=orm['fantasy_league.Team'], null=True, blank=True)),
         ))
         db.send_create_signal(u'fantasy_league', ['User'])
+
+        # Adding M2M table for field groups on 'User'
+        db.create_table(u'fantasy_league_user_groups', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('user', models.ForeignKey(orm[u'fantasy_league.user'], null=False)),
+            ('group', models.ForeignKey(orm[u'auth.group'], null=False))
+        ))
+        db.create_unique(u'fantasy_league_user_groups', ['user_id', 'group_id'])
+
+        # Adding M2M table for field user_permissions on 'User'
+        db.create_table(u'fantasy_league_user_user_permissions', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('user', models.ForeignKey(orm[u'fantasy_league.user'], null=False)),
+            ('permission', models.ForeignKey(orm[u'auth.permission'], null=False))
+        ))
+        db.create_unique(u'fantasy_league_user_user_permissions', ['user_id', 'permission_id'])
 
 
     def backwards(self, orm):
@@ -81,11 +107,37 @@ class Migration(SchemaMigration):
         # Deleting model 'User'
         db.delete_table(u'fantasy_league_user')
 
+        # Removing M2M table for field groups on 'User'
+        db.delete_table('fantasy_league_user_groups')
+
+        # Removing M2M table for field user_permissions on 'User'
+        db.delete_table('fantasy_league_user_user_permissions')
+
 
     models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
         u'fantasy_league.membership': {
             'Meta': {'object_name': 'Membership'},
-            'amount_paid': ('django.db.models.fields.DecimalField', [], {'max_digits': '3', 'decimal_places': '2'}),
+            'amount_paid': ('django.db.models.fields.DecimalField', [], {'max_digits': '4', 'decimal_places': '2'}),
             'date_picked': ('django.db.models.fields.DateField', [], {}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'player': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['fantasy_league.Player']"}),
@@ -94,9 +146,9 @@ class Migration(SchemaMigration):
         },
         u'fantasy_league.player': {
             'Meta': {'object_name': 'Player'},
-            'current_value': ('django.db.models.fields.DecimalField', [], {'max_digits': '3', 'decimal_places': '2'}),
+            'current_value': ('django.db.models.fields.DecimalField', [], {'max_digits': '4', 'decimal_places': '2'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'init_value': ('django.db.models.fields.DecimalField', [], {'max_digits': '3', 'decimal_places': '2'}),
+            'init_value': ('django.db.models.fields.DecimalField', [], {'max_digits': '4', 'decimal_places': '2'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'positions': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'positions'", 'symmetrical': 'False', 'to': u"orm['fantasy_league.Position']"}),
             'primary_position': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'primary_position'", 'to': u"orm['fantasy_league.Position']"})
@@ -110,15 +162,27 @@ class Migration(SchemaMigration):
             'Meta': {'object_name': 'Team'},
             'captain': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'captain'", 'to': u"orm['fantasy_league.Player']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'money_to_spend': ('django.db.models.fields.DecimalField', [], {'max_digits': '6', 'decimal_places': '2'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
             'players': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'players'", 'symmetrical': 'False', 'through': u"orm['fantasy_league.Membership']", 'to': u"orm['fantasy_league.Player']"}),
-            'team_points': ('django.db.models.fields.DecimalField', [], {'max_digits': '4', 'decimal_places': '2'})
+            'team_points': ('django.db.models.fields.DecimalField', [], {'max_digits': '6', 'decimal_places': '2'})
         },
         u'fantasy_league.user': {
             'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'team': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['fantasy_league.Team']"})
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'team': ('django.db.models.fields.related.ForeignKey', [], {'default': 'None', 'to': u"orm['fantasy_league.Team']", 'null': 'True', 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '254'})
         }
     }
 
